073937a93eb9d59017d43fe7180320e43171e2dc
Brian J Brennan <brianloveswords@gmail.com>
2013-11-27 01:19:20 -0500

    SEARCH FX LOL

--- static/script.js
+++ static/script.js
@@ -2,19 +2,47 @@ const entries = [].slice.call(document.querySelectorAll('li'))
 const filter = document.getElementById('filter')
 
 var oldTerm
-setInterval(function () {
+filter.addEventListener('keyup', search)
 
-  const term = filter.value
+function search() {
+  const term = filter.value.trim()
+
   if (term == oldTerm) return
+
+  if (!term)
+    return showAll(entries)
+
   oldTerm = term
 
   console.log('doing')
-  const pattern = new RegExp('.*?' + term + '.*?', 'i')
+  const pattern = new RegExp('.*?(' + term + ').*?', 'i')
   entries.forEach(function (entry) {
+    const link = entry.querySelector('a')
     const filename = entry.dataset.file
-    if (!term || pattern.test(filename))
+    const match = pattern.exec(filename)
+    if (pattern.test(filename)) {
+      link.innerHTML =
+        filename.replace(match[1], '<span>'+match[1]+'</span>')
+
+      link.querySelector('span').classList.add('highlight')
       entry.classList.remove('hidden')
-    else
+    }
+    else {
+      link.innerHTML = filename
       entry.classList.add('hidden')
+    }
   })
-}, 300)
+}
+
+function showAll(entries) {
+  entries.forEach(function (entry) {
+    const link = entry.querySelector('a')
+    const filename = entry.dataset.file
+    entries.forEach(function (entry) {
+      link.innerHTML = filename
+      entry.classList.remove('hidden')
+    })
+  })
+}
+
+search()
