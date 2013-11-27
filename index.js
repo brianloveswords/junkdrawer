b358d0cdefdeacc02f9dbeac565c545aace13259
Brian J Brennan <brianloveswords@gmail.com>
2013-11-26 20:45:15 -0500

    Copying, Moving and Deleting.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const path = require('path')
 const EventEmitter = require('events').EventEmitter
 const fs = require('fs')
 const safepath = require('./lib/safepath')
 const util = require('util')
 const xtend = require('xtend')
+const through = require('through')
 
 module.exports = JunkDrawer
 
@@ -12,41 +14,127 @@ function JunkDrawer(opts) {
   if (!(this instanceof JunkDrawer))
     return new JunkDrawer(opts)
 
-  this._emit = EventEmitter.prototype.emit
+  defer(this, 'emit')
   this.root = opts.root || '.'
-  this.path = opts.path || 'junk'
 }
 
-
 JunkDrawer.prototype = xtend(EventEmitter.prototype, {
-  emit: function () {
-    const self = this
-    const args = arguments
-    global.setImmediate(function () {
-      self._emit.apply(self, args)
+  doneEmitter: function (callback) {
+    const ee = new EventEmitter()
+    if (callback) {
+      ee.on('error', callback)
+      ee.on('done', callback)
+    }
+    defer(ee, 'emit')
+    return ee
+  },
+
+  safe: function (file) {
+    return safepath(this.root, file)
+  },
+
+  createStream: function(type, file) {
+    const stream = through()
+    const safename = this.safe(file)
+    defer(stream, 'emit')
+
+    if (!safename)
+      return this.unsafeAccessError(type, file, stream)
+
+    this.emit('begin' + type)
+    const readOrWrite = 'create' + type + 'Stream'
+    return fs[readOrWrite](safename)
+  },
+  createWriteStream: function (file) {
+    return this.createStream('Write', file)
+  },
+  createReadStream: function (file) {
+    return this.createStream('Read', file)
+  },
+  copyFile: function (from, to, callback) {
+    const event = this.doneEmitter(callback)
+
+    const safefrom = this.safe(from)
+    const safeto = this.safe(to)
+    if (!safefrom) return this.unsafeAccessError('Copy', from, event)
+    if (!safeto) return this.unsafeAccessError('Copy', to, event)
+
+    this.createReadStream(from)
+      .on('error', function (error) { event.emit('error', error) })
+      .pipe(this.createWriteStream(to))
+      .on('error', function (error) { event.emit('error', error) })
+      .on('finish', function () { event.emit('done') })
+
+    return event
+  },
+  deleteFile: function (file, callback) {
+    const event = this.doneEmitter(callback)
+    const safename = this.safe(file)
+
+    if (!safename)
+      return this.unsafeAccessError('Delete', file, event)
+
+    fs.unlink(safename, function (error) {
+      if (error)
+        return event.emit('error', error)
+      return event.emit('done')
     })
-  },
-  safe: function (filename) {
-    return safepath(this.root, filename)
-  },
-  stream: function(type, filename) {
-    const tr = {
-      write: 'Write',
-      read: 'Read'
-    }
 
-    if (!this.safe(filename)) {
-      this.emit('unsafe'+tr[type], filename)
-      return false
-    }
+    return event
+  },
+  moveFile: function (from, to, callback) {
+    const self = this
+    const event = self.doneEmitter(callback)
+    const safefrom = self.safe(from)
+    const safeto = self.safe(to)
 
-    return fs['create'+tr[type]+'Stream'](this.safe(filename))
+    if (!safefrom) return self.unsafeAccessError('Move', from, event)
+    if (!safeto) return self.unsafeAccessError('Move', to, event)
+
+    self.copyFile(safefrom, safeto, function (error) {
+      if (error) return event.emit('error', error)
+
+      self.deleteFile(safefrom, function (error) {
+        if (error) return event.emit('error', error)
+        return event.emit('done')
+      })
+    })
+
+    return event
   },
-  createWriteStream: function (filename) {
-    return this.stream('write', filename)
-  },
-  createReadStream: function (filename) {
-    return this.stream('read', filename)
-  },
+  unsafeAccessError: function (type, file, emitter) {
+    const error = unsafeAccessError(this.root, file)
+    this.emit('unsafe' + type, error)
+    emitter.emit('error', error)
+    return emitter
+  }
 
 })
+
+function defer(obj, method) {
+  const orig = obj[method]
+  obj[method] = function () {
+    const args = arguments
+    global.setImmediate(function () {
+      orig.apply(obj, args)
+    })
+  }
+}
+
+function unsafeAccessError(root, filename) {
+  const resolved = path.resolve(root, filename)
+  const error = new Error('outside of junk drawer, ' + resolved)
+  error.name = 'UnsafeAccess'
+  error.code = 'EUNSAFEACCESS'
+  error.root = root
+  error.path = resolved
+  error.filename = filename
+  error.stack = fixStack(error.stack)
+  return error
+}
+
+function fixStack(stack) {
+  const lines = stack.split('\n')
+  const first = lines[0]
+  return [first].concat(lines.slice(2)).join('\n')
+}
--- test/junkdrawer.test.js
+++ test/junkdrawer.test.js
@@ -1,34 +1,113 @@
-const path = require('path')
 const fs = require('fs')
+const path = require('path')
+const concat = require('concat-stream')
 const JunkDrawer = require('..')
 
 const test = require('tap').test
-const junk = new JunkDrawer({ path: 'junk' })
+const junkDir = path.join(__dirname, 'junk')
+const junk = new JunkDrawer({ root: junkDir })
 
 test('junkdrawer writes', function (t) {
-  const writeStream = junk.createWriteStream('some-trash.png')
+  var writeStream
+  t.plan(3)
+
+  writeStream = junk.createWriteStream('some-trash.png')
   t.ok(writeStream, 'should have junkstream')
 
-  t.notOk(junk.createWriteStream('../some/bullshit'), 'no sstream')
-  junk.once('unsafeWrite', function (filename) {
-    t.same(filename, '../some/bullshit')
+  writeStream = junk.createWriteStream('../some/bullshit')
+  junk.once('unsafeWrite', function (error) {
+    t.same(error.filename, '../some/bullshit')
   })
-
-  t.end()
+  writeStream.once('error', function (error) {
+    t.same(error.name, 'UnsafeAccess')
+  })
 })
 
 test('junkdrawer reads', function (t) {
-  const readStream = junk.createReadStream('dope.png')
+  var readStream
+  t.plan(3)
+
+  readStream = junk.createReadStream('flan.jpg')
   t.ok(readStream, 'should have junkstream')
 
-  t.notOk(junk.createReadStream('../some/bullshit'), 'no sstream')
-  junk.once('unsafeRead', function (filename) {
-    t.same(filename, '../some/bullshit')
+  readStream = junk.createReadStream('../some/bullshit')
+  junk.once('unsafeRead', function (error) {
+    t.same(error.filename, '../some/bullshit')
   })
-
-  t.end()
+  readStream.once('error', function (error) {
+    t.same(error.name, 'UnsafeAccess')
+  })
 })
 
+test('junkdrawer copy', function (t) {
+  t.plan(3)
+
+  junk.copyFile('flan.jpg', 'such-flan.jpg')
+    .on('done', function () {
+      t.same(read('flan.jpg'), read('such-flan.jpg'))
+    })
+
+
+  junk.copyFile('../../../flan.jpg', 'such-flan.jpg')
+    .on('error', function (error) {
+      t.same(error.name, 'UnsafeAccess')
+    })
+
+  junk.copyFile('flan.jpg', '../../../flan.jpg')
+    .on('error', function (error) {
+      t.same(error.name, 'UnsafeAccess')
+    })
+
+})
+
+test('junkdrawer delete', function (t) {
+  t.plan(3)
+
+  junk.deleteFile('such-flan.jpg')
+    .on('done', function () {
+      t.notOk(exists('such-flan.jpg'), 'deleted')
+    })
+
+  junk.deleteFile('such-flan.jpg')
+    .on('error', function (error) {
+      t.same(error.code, 'ENOENT', 'does not exist')
+    })
+
+  junk.deleteFile('../such-flan.jpg')
+    .on('error', function (error) {
+      t.same(error.code, 'EUNSAFEACCESS', 'unsafe')
+    })
+})
+
+test('junkdrawer move', function (t) {
+  cp('flan.jpg', 'such-flan.jpg')
+
+  junk.moveFile('such-flan.jpg', 'wow-flan.jpg')
+    .on('done', function () {
+      t.ok(exists('wow-flan.jpg'), 'should exist')
+      t.notOk(exists('such-flan.jpg'), 'should not exist')
+      unlink('wow-flan.jpg')
+      t.end()
+    })
+})
+
+function cp(from, to) {
+  const data = fs.readFileSync(path.join(junkDir, from))
+  fs.writeFileSync(path.join(junkDir, to), data)
+}
+
+function exists(file) {
+  return fs.existsSync(path.join(junkDir, file))
+}
+
+function unlink(file) {
+  return fs.unlinkSync(path.join(junkDir, file))
+}
+
 function stream(file) {
-  return fs.createReadStream(path.join(__dirname, 'data', file))
+  return fs.createReadStream(path.join(junkDir, file))
+}
+
+function read(file) {
+  return fs.readFileSync(path.join(junkDir, file))
 }
