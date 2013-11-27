d6ca03499902f54c4fa56f9942d2956dd40327df
Brian J Brennan <brianloveswords@gmail.com>
2013-11-27 00:57:26 -0500

    Better performance

--- static/script.js
+++ static/script.js
@@ -1,16 +1,20 @@
 const entries = [].slice.call(document.querySelectorAll('li'))
-
-
 const filter = document.getElementById('filter')
 
-filter.addEventListener('keyup', function (evt) {
-  const term = this.value
-  const pattern = new RegExp('.*?' + this.value + '.*?')
+var oldTerm
+setInterval(function () {
+
+  const term = filter.value
+  if (term == oldTerm) return
+  oldTerm = term
+
+  console.log('doing')
+  const pattern = new RegExp('.*?' + term + '.*?', 'i')
   entries.forEach(function (entry) {
     const filename = entry.dataset.file
-    if (!pattern.test(filename))
-      entry.classList.add('hidden')
-    else
+    if (!term || pattern.test(filename))
       entry.classList.remove('hidden')
+    else
+      entry.classList.add('hidden')
   })
-})
+}, 300)
