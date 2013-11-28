fb382f232cbe73b45206ab29f09d1ede1d12afc2
Brian J Brennan <brianloveswords@gmail.com>
2013-11-27 20:17:51 -0500

    Optimize for lots of stuff.

--- static/script.js
+++ static/script.js
@@ -1,122 +1,65 @@
-const list = document.querySelector('ul')
-const entries = [].slice.call(document.querySelectorAll('li'))
-const filter = document.getElementById('filter')
+/*!
+* domready (c) Dustin Diaz 2012 - License MIT
+*/
+!function(e,t){typeof module!="undefined"?module.exports=t():typeof define=="function"&&typeof define.amd=="object"?define(t):this[e]=t()}("domready",function(e){function p(e){h=1;while(e=t.shift())e()}var t=[],n,r=!1,i=document,s=i.documentElement,o=s.doScroll,u="DOMContentLoaded",a="addEventListener",f="onreadystatechange",l="readyState",c=o?/^loaded|^c/:/^loaded|c/,h=c.test(i[l]);return i[a]&&i[a](u,n=function(){i.removeEventListener(u,n,r),p()},r),o&&i.attachEvent(f,n=function(){/^c/.test(i[l])&&(i.detachEvent(f,n),p())}),e=o?function(n){self!=top?h?n():t.push(n):function(){try{s.doScroll("left")}catch(t){return setTimeout(function(){e(n)},50)}n()}()}:function(e){h?e():t.push(e)}})
 
-var oldTerm
-filter.addEventListener('keyup', search)
+window.domready(function () {
+  var entries = [].slice.call(document.querySelectorAll('li'))
+  const list = document.querySelector('ul')
+  const filter = document.getElementById('filter')
 
-var focused = -2
+  const cache = list.innerHTML
 
-document.body.addEventListener('keydown', keystrokes, true)
+  filter.addEventListener('keyup', search)
 
-function keystrokes(event) {
-  // const up = 38
-  // const down = 40
-
-  // var visible = [].slice.call(document.querySelectorAll('li:not(.hidden)'))
-
-  // if (!visible.length)
-  //   return filter.focus()
-
-  // if (event.keyCode == down) {
-  //   focused ++
-
-  //   if (focused == -1)
-  //     return filter.focus()
-
-  //   if (focused == visible.length)
-  //     focused = visible.length - 1
-  //   visible[focused].querySelector('a').focus()
-  //   return false
-  // }
-
-  // else if (event.keyCode == up) {
-  //   if (focused == -1)
-  //     return
-
-  //   focused --
-
-  //   if (focused < 0) {
-  //     list.querySelector('a:focus').blur()
-  //     filter.focus()
-  //     focused = -1
-  //     return
-  //   }
-  //   visible[focused].querySelector('a').focus()
-  //   return false
-  // }
-
-  // else {
-  //   filter.focus()
-  //   return true
-  // }
-}
-
-
-function search(event) {
-  const up = 38
-  const down = 40
-
-  if (event.keyCode == down || event.keyCode == up)
-    return keystrokes(event)
-
-
-  const term = filter.value.trim()
-
-  if (term == oldTerm) return
-
-  if (!term)
-    return showAll(entries)
-
-  oldTerm = term
-
-
-  const pattern = new RegExp('.*?(' + term + ').*?', 'i')
-  entries.forEach(function (entry) {
-    const link = entry.querySelector('a')
-    const filename = entry.dataset.file
-    const match = pattern.exec(filename)
-    if (pattern.test(filename)) {
-      entry.classList.remove('hidden')
-
-      const highlight = link.querySelector('span')
-
-      function addHighlight() {
-        clearTimeout(entry.timerAlpha)
-        link.innerHTML =
-          filename.replace(match[1], '<span>'+match[1]+'</span>')
-        entry.timerAlpha = setTimeout(function () {
-          link.querySelector('span').classList.add('highlight')
-        }, 50)
-      }
-
-      if (highlight) {
-        clearTimeout(entry.timerBeta)
-        highlight.classList.remove('highlight')
-        entry.timerBeta = setTimeout(addHighlight, 400)
-      }
-
-      else
-        addHighlight()
-
-
-    }
-    else {
-      link.innerHTML = filename
-      entry.classList.add('hidden')
-    }
-  })
-}
-
-function showAll(entries) {
-  entries.forEach(function (entry) {
-    const link = entry.querySelector('a')
-    const filename = entry.dataset.file
+  var oldTerm
+  function search(event) {
+    const term = filter.value.trim()
+    if (term == oldTerm) return
+    if (!term && oldTerm)
+      return showAll(entries)
+    oldTerm = term
+    const pattern = new RegExp('.*?(' + term + ').*?', 'i')
     entries.forEach(function (entry) {
-      link.innerHTML = filename
-      entry.classList.remove('hidden')
-    })
-  })
-}
+      const link = entry.querySelector('a')
+      const filename = entry.dataset.file
+      const match = pattern.exec(filename)
 
-search({})
+      if (pattern.test(filename)) {
+        const highlight = link.querySelector('span')
+
+        entry.classList.remove('hidden')
+
+        function addHighlight() {
+          clearTimeout(entry.timerAlpha)
+          link.innerHTML =
+            filename.replace(match[1], '<span>'+match[1]+'</span>')
+          entry.timerAlpha = setTimeout(function () {
+            link.querySelector('span').classList.add('highlight')
+          }, 50)
+        }
+
+        if (highlight) {
+          clearTimeout(entry.timerBeta)
+          highlight.classList.remove('highlight')
+          entry.timerBeta = setTimeout(addHighlight, 400)
+        }
+
+        else
+          addHighlight()
+      }
+
+      else {
+        link.innerHTML = filename
+        entry.classList.add('hidden')
+      }
+    })
+  }
+
+  function showAll() {
+    list.innerHTML = cache
+    entries = [].slice.call(document.querySelectorAll('li'))
+  }
+
+  search({})
+})
